f62b2995681e7e4f80b7a81637c62d7928d9a124
Ian Chu <chuiantw1212@gmail.com>
2023-06-20 11:25:19 +0800

    remove dist

--- app.js
+++ app.js
@@ -6,32 +6,30 @@ import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 import { AutoImage } from '@ckeditor/ckeditor5-image';
 import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
 import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
+import DocumentList from '@ckeditor/ckeditor5-list/src/documentlist.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
-import SelectAll from '@ckeditor/ckeditor5-select-all/src/selectall.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
 ClassicEditor
     .create(document.querySelector('#editor'), {
         plugins: [
-            Essentials, 
-            Paragraph, 
-            Heading, 
+            Essentials,
+            Paragraph,
+            Heading,
             AutoImage,
             AutoLink,
             DataFilter,
+            DocumentList,
             Image,
-            Link, 
-            List, 
-            MediaEmbed, 
+            Link,
+            MediaEmbed,
             MediaEmbedToolbar,
             PasteFromOffice,
             RemoveFormat,
-            SelectAll,
             WordCount,
             EditorWatchdog,
         ],
@@ -51,6 +49,7 @@ ClassicEditor
                 'removeFormat'
             ]
         },
+        language: 'zh',
     })
     .then(editor => {
         console.log('Editor was initialized', editor);
@@ -58,3 +57,6 @@ ClassicEditor
     .catch(error => {
         console.error(error.stack);
     });
+// https://stackoverflow.com/questions/50657924/getting-classiceditor-exported-on-custom-build-of-ckeditor-5
+window.ClassicEditor = ClassicEditor;
+export default ClassicEditor;
