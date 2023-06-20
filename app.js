1be852c52d34d09d7062bd6c31926f9cbda2ebd7
Ian Chu <chuiantw1212@gmail.com>
2023-06-20 13:32:04 +0800

    add bold italic

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
 // Add custom features
 import { AutoImage } from '@ckeditor/ckeditor5-image';
 import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
-import DocumentList from '@ckeditor/ckeditor5-list/src/documentlist.js';
+import DocumentList from '@ckeditor/ckeditor5-list/src/documentlist.js'
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';;
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+// import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
@@ -24,13 +26,15 @@ Editor.builtinPlugins = [
     Heading,
     AutoImage,
     AutoLink,
+    Bold,
     DataFilter,
     DocumentList,
     Image,
     ImageUpload,
+    Italic,
     Link,
     MediaEmbed,
-    MediaEmbedToolbar,
+    // MediaEmbedToolbar,
     PasteFromOffice,
     RemoveFormat,
     WordCount,
@@ -43,6 +47,8 @@ Editor.defaultConfig = {
         items: [
             'heading',
             '|',
+            'bold',
+            'italic',
             'link',
             'bulletedList',
             'numberedList',
