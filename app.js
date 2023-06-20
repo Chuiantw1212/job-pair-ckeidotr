dca7a3daba58e858d2ca9cc66650969a1630d878
Ian Chu <chuiantw1212@gmail.com>
2023-06-20 13:14:39 +0800

    update code example

--- app.js
+++ app.js
@@ -8,55 +8,55 @@ import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
 import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
 import DocumentList from '@ckeditor/ckeditor5-list/src/documentlist.js';
 import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-// import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
+import MediaEmbedToolbar from '@ckeditor/ckeditor5-media-embed/src/mediaembedtoolbar.js';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
-ClassicEditor
-    .create(document.querySelector('#editor'), {
-        plugins: [
-            Essentials,
-            Paragraph,
-            Heading,
-            AutoImage,
-            AutoLink,
-            DataFilter,
-            DocumentList,
-            Image,
-            Link,
-            MediaEmbed,
-            // MediaEmbedToolbar,
-            PasteFromOffice,
-            RemoveFormat,
-            WordCount,
-            EditorWatchdog,
-        ],
-        toolbar: {
-            items: [
-                'heading',
-                '|',
-                'link',
-                'bulletedList',
-                'numberedList',
-                '|',
-                'imageUpload',
-                'mediaEmbed',
-                'undo',
-                'redo',
-                '|',
-                'removeFormat'
-            ]
-        },
-        language: 'zh',
-    })
-    .then(editor => {
-        console.log('Editor was initialized', editor);
-    })
-    .catch(error => {
-        console.error(error.stack);
-    });
+
+class Editor extends ClassicEditor { }
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+    Essentials,
+    Paragraph,
+    Heading,
+    AutoImage,
+    AutoLink,
+    DataFilter,
+    DocumentList,
+    Image,
+    ImageUpload,
+    Link,
+    MediaEmbed,
+    MediaEmbedToolbar,
+    PasteFromOffice,
+    RemoveFormat,
+    WordCount,
+    EditorWatchdog,
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+    toolbar: {
+        items: [
+            'heading',
+            '|',
+            'link',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'imageUpload',
+            'mediaEmbed',
+            'undo',
+            'redo',
+            '|',
+            'removeFormat'
+        ]
+    },
+    language: 'zh',
+};
 // https://stackoverflow.com/questions/50657924/getting-classiceditor-exported-on-custom-build-of-ckeditor-5
-window.ClassicEditor = ClassicEditor;
-export default ClassicEditor;
+window.ClassicEditor = Editor;
+export default Editor;
