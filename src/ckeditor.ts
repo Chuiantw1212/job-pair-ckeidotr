b150182b2be2738b000da1a8a4d4574551be7077
Chuian <chuiantw1212@gmail.com>
2023-08-26 19:43:08 +0800

    use online builder

---
+++ src/ckeditor.ts
@@ -0,0 +1,65 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontColor, FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { DataFilter } from '@ckeditor/ckeditor5-html-support';
+import { Image, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Link, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+
+// You can read more about extending the build with additional plugins in the "Installing plugins" guide.
+// See https://ckeditor.com/docs/ckeditor5/latest/installation/plugins/installing-plugins.html for details.
+
+class Editor extends ClassicEditor {
+	public static override builtinPlugins = [
+		Bold,
+		DataFilter,
+		Essentials,
+		FontColor,
+		FontSize,
+		Heading,
+		Image,
+		ImageUpload,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		MediaEmbed,
+		Paragraph,
+		PasteFromOffice,
+		RemoveFormat
+	];
+
+	public static override defaultConfig = {
+		toolbar: {
+			items: [
+				'heading',
+				'|',
+				'bold',
+				'italic',
+				'link',
+				'bulletedList',
+				'numberedList',
+				'|',
+				'imageUpload',
+				'mediaEmbed',
+				'undo',
+				'redo'
+			]
+		},
+		language: 'zh'
+	};
+}
+
+export default Editor;
