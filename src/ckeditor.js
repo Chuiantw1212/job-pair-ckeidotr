fb1aacbdc6a1734d96154aed3f72981e23ecf6df
Chuian <chuiantw1212@gmail.com>
2023-09-07 14:16:34 +0800

    attempt super build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,15 +1,39 @@
 // The editor creators to use.
 import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
 import { InlineEditor as InlineEditorBase } from '@ckeditor/ckeditor5-editor-inline';
+// Plugins
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import {
+    Image,
+} from '@ckeditor/ckeditor5-image';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
 
-// ...
-
-class ClassicEditor extends ClassicEditorBase {}
-class InlineEditor extends InlineEditorBase {}
+class ClassicEditor extends ClassicEditorBase { }
+class InlineEditor extends InlineEditorBase { }
 
 // Plugins to include in the build.
 const plugins = [
-    // ...
+    Alignment,
+    Bold,
+    Essentials,
+    Heading,
+    Image,
+    Indent,
+    Italic,
+    Link,
+    List,
+    MediaEmbed,
+    Paragraph,
+    PasteFromOffice,
 ];
 
 ClassicEditor.builtinPlugins = plugins;
@@ -17,7 +41,31 @@ InlineEditor.builtinPlugins = plugins;
 
 // Editor configuration.
 const config = {
-    // ...
+    toolbar: {
+        items: [
+            'undo',
+            'redo',
+            '|',
+            'heading',
+            '|',
+            'fontSize',
+            '|',
+            'bold',
+            'italic',
+            'fontColor',
+            '|',
+            'link',
+            'imageUpload',
+            'mediaEmbed',
+            '|',
+            'alignment',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'removeFormat'
+        ]
+    },
+    language: 'zh',
 };
 
 ClassicEditor.defaultConfig = config;
