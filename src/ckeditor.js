76ff259d01b6ecec300999067744c75f3ebd24f0
Chuian <chuiantw1212@gmail.com>
2025-01-24 08:10:18 +0800

    votion client

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,15 +1,21 @@
-// The editor creators to use.
+// The editor creator to use.
 import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
 import { InlineEditor as InlineEditorBase } from '@ckeditor/ckeditor5-editor-inline';
+
 // Plugins
 import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
 import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { Font, FontFamily } from '@ckeditor/ckeditor5-font';
-import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
 import { Heading } from '@ckeditor/ckeditor5-heading';
 import {
     Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload
 } from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
 import { Link } from '@ckeditor/ckeditor5-link';
@@ -17,22 +23,25 @@ import { List } from '@ckeditor/ckeditor5-list';
 import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 
 class ClassicEditor extends ClassicEditorBase { }
 class InlineEditor extends InlineEditorBase { }
 
-// Plugins to include in the build.
 const plugins = [
     Alignment,
+    Autoformat,
+    BlockQuote,
     Bold,
-    // DataFilter,
+    CloudServices,
     Essentials,
-    Font,
-    FontFamily,
-    GeneralHtmlSupport,
     Heading,
     Image,
+    ImageCaption,
+    ImageStyle,
+    ImageToolbar,
+    ImageUpload,
     Indent,
     Italic,
     Link,
@@ -40,49 +49,57 @@ const plugins = [
     MediaEmbed,
     Paragraph,
     PasteFromOffice,
-    RemoveFormat,
-];
+    Table,
+    TableToolbar,
+    TextTransformation,
+]
 
 ClassicEditor.builtinPlugins = plugins;
 InlineEditor.builtinPlugins = plugins;
 
-// Editor configuration.
 const config = {
     toolbar: {
         items: [
-            'undo',
-            'redo',
-            '|',
+            'alignment',
             'heading',
-            '|',
-            'fontSize',
             '|',
             'bold',
             'italic',
-            'fontColor',
-            '|',
             'link',
-            'imageUpload',
-            'mediaEmbed',
-            '|',
-            'alignment',
             'bulletedList',
             'numberedList',
             '|',
-            'removeFormat'
+            'outdent',
+            'indent',
+            '|',
+            'imageUpload',
+            'blockQuote',
+            'insertTable',
+            'mediaEmbed',
+            'undo',
+            'redo'
         ]
     },
-    language: 'zh',
-    // // https://ckeditor.com/docs/ckeditor5/latest/features/html/general-html-support.html
-    // allow: [
-    //     {
-    //         name: /.*/,
-    //         attributes: true,
-    //         classes: true,
-    //         styles: true
-    //     }
-    // ],
-};
+
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: 'zh_TW',
+    image: {
+        toolbar: [
+            'imageTextAlternative',
+            'toggleImageCaption',
+            'imageStyle:inline',
+            'imageStyle:block',
+            'imageStyle:side'
+        ]
+    },
+    table: {
+        contentToolbar: [
+            'tableColumn',
+            'tableRow',
+            'mergeTableCells'
+        ]
+    }
+}
 
 ClassicEditor.defaultConfig = config;
 InlineEditor.defaultConfig = config;
